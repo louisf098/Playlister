c8b1ded48a22be3e099d03bd6508a7c21c6651ff
Louis <louis098765@gmail.com>
2022-11-29 00:02:15 -0500

    Status bar does not appear on login/register/splash screens

--- client/src/components/Statusbar.js
+++ client/src/components/Statusbar.js
@@ -1,10 +1,12 @@
 import { useContext } from 'react'
 import { GlobalStoreContext } from '../store'
+import AuthContext from '../auth';
+
 import { Typography } from '@mui/material'
-
+import Box from '@mui/material/Box';
 import Fab from '@mui/material/Fab'
-import IconButton from '@mui/material/IconButton';
 
+import IconButton from '@mui/material/IconButton';
 import AddIcon from '@mui/icons-material/Add';
 /*
     Our Status bar React component goes at the bottom of our UI.
@@ -12,6 +14,7 @@ import AddIcon from '@mui/icons-material/Add';
     @author McKilla Gorilla
 */
 function Statusbar() {
+    const { auth } = useContext(AuthContext);
     const { store } = useContext(GlobalStoreContext);
 
     function handleCreateNewList() {
@@ -21,19 +24,34 @@ function Statusbar() {
     let text ="";
     if (store.currentList)
         text = store.currentList.name;
+
+    let statusBar = "";
+    if (auth.loggedIn) {
+        console.log("hi");
+        statusBar =
+            <div id="playlister-statusbar">
+                {/* <Typography variant="h4">{text}</Typography> */}
+                
+                <IconButton 
+                    aria-label="add"
+                    id="add-list-button"
+                    onClick={handleCreateNewList}
+                >
+                    <AddIcon sx={{ fontSize: "35pt", color: "black"}}/>
+                </IconButton>
+                    <Typography variant="h2">Your Lists</Typography>
+                
+            </div>
+        
+    }
+    else {
+        statusBar = <Box></Box>
+    }
+        
+
     return (
-        <div id="playlister-statusbar">
-            {/* <Typography variant="h4">{text}</Typography> */}
-            
-            <IconButton 
-                aria-label="add"
-                id="add-list-button"
-                onClick={handleCreateNewList}
-            >
-                <AddIcon sx={{ fontSize: "35pt", color: "black"}}/>
-            </IconButton>
-                <Typography variant="h2">Your Lists</Typography>
-            
+        <div>
+            {statusBar}
         </div>
     );
 }

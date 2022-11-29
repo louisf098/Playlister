76ca2f5bfec287f336c48241aa7c2867a08e22c5
Louis <louis098765@gmail.com>
2022-11-28 22:31:05 -0500

    Splash screen

--- client/src/components/SplashScreen.js
+++ client/src/components/SplashScreen.js
@@ -1,7 +1,73 @@
+import Box from "@mui/material/Box";
+import { Typography } from "@mui/material";
+import Grid from "@mui/material/Grid";
+import Button from "@mui/material/Button";
+
 export default function SplashScreen() {
+    let buttonStyles = {
+        "&.MuiButton-contained": {
+            backgroundImage: "linear-gradient(to bottom, #24125b, purple)",
+        },
+        padding: '15px',
+        width: '115px',
+        height: '80px',
+        fontFamily: "'Merriweather', serif"
+    };
+
     return (
-        <div id="splash-screen">
-            Playlister
-        </div>
-    )
+        <Box id="splash-screen" sx={{ flexGrow: 1 }}>
+            <Grid
+                container
+                spacing={1}
+                sx={{ alignContent: "center", justifyContent: "center" }}
+            >
+                <Grid item xs={12}>
+                    <Typography
+                        sx={{
+                            fontFamily: "'Satisfy', cursive",
+                            fontSize: "6vw",
+                        }}
+                    >
+                        Playlister
+                    </Typography>
+                </Grid>
+                <Grid item xs={12}>
+                    <Typography
+                        sx={{
+                            fontWeight: "bold",
+                            fontSize: "2vw",
+                            fontStyle: "italic",
+                            textDecoration: "underline",
+                        }}
+                    >
+                        Welcome to Playlister!
+                    </Typography>
+                </Grid>
+                <Grid item xs={6}>
+                    <Typography sx={{ fontFamily: "'Merriweather', serif" }}>
+                        Create, edit, and play your very own playlists through
+                        playlister! Tired of your own songs? Search for your
+                        favorite playlists created by other users!
+                    </Typography>
+                </Grid>
+                <Grid container>
+                    <Grid item xs={4}>
+                        <Button variant="contained" sx={buttonStyles}>
+                            Login
+                        </Button>
+                    </Grid>
+                    <Grid item xs={4}>
+                        <Button variant="contained" sx={buttonStyles}>
+                            Create Account
+                        </Button>
+                    </Grid>
+                    <Grid item xs={4}>
+                        <Button variant="contained" sx={buttonStyles}>
+                            Login as Guest
+                        </Button>
+                    </Grid>
+                </Grid>
+            </Grid>
+        </Box>
+    );
 }
